f1ead18c172389dec551b042bdb60c7bb176fa47
devMali <malidevendra40@gmail.com>
2022-08-29 20:20:37 +0530

    Updates

---
+++ precache-manifest.5dfa2eb3de65bb604250ab9f44808c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7826762b5962b2ba413",
+    "url": "/dev-app/static/css/main.4beb7cda.chunk.css"
+  },
+  {
+    "revision": "a7826762b5962b2ba413",
+    "url": "/dev-app/static/js/main.a7826762.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/dev-app/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "75e706a8dedb5add3e8d",
+    "url": "/dev-app/static/js/2.75e706a8.chunk.js"
+  },
+  {
+    "revision": "e79cc6ac8f68a95c9f64",
+    "url": "/dev-app/static/js/runtime~main.e79cc6ac.js"
+  },
+  {
+    "revision": "1f3d8cb84bbdd876a9aca4b8e9566411",
+    "url": "/dev-app/index.html"
+  }
+];
